8f73ab888a1a48dedec509cde26427f7e9c95b30
mechztcc <dojathan@gmail.com>
2025-04-23 08:58:26 -0300

    feat: add together.ai implementation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,25 @@
-import express from 'express'
+import express from 'express';
+import Together from 'together-ai';
+import 'dotenv/config';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API com TypeScript e Express 🚀')
-})
+app.get('/', async (req, res) => {
+  const together = new Together({
+    apiKey: process.env.TOGETHER_API_KEY,
+  });
+
+  const response = await together.chat.completions.create({
+    messages: [
+      { role: 'user', content: 'What are some fun things to do in New York?' },
+    ],
+    model: 'meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8',
+  });
+
+  res.send(response.choices[0].message);
+});
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000')
-})
+  console.log('Servidor rodando em http://localhost:3000');
+});
